8ea91c03ffefa820fd5d16c19db18815a4ee6d79
Jaimedgp <jaime.diez.gp@gmail.com>
2018-10-30 12:08:36 +0100

    Mejorandolo

--- Fotonica/ResonadorTrue.py
+++ Fotonica/ResonadorTrue.py
@@ -1,12 +1,29 @@
-from Pupila import EspejoResonador
 import numpy as np
 import matplotlib.pyplot as plt
+import threading
+
+from Pupila import EspejoResonador
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.misc import imread, imresize, imrotate
 
+def mostrarEspejo(x, y, matrix):
+    # create the figure
+    fig = plt.figure(figsize=plt.figaspect(0.5))
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax2 = fig.add_subplot(1, 2, 2, projection='3d')
 
-Espejo1 = EspejoResonador(curvatura=0.01)
-Espejo2 = EspejoResonador(curvatura=0.02)
+    # Mostrar la imagen rotandola
+    ax1.imshow(abs(matrix), cmap=plt.cm.jet, interpolation='nearest',
+                origin='lower', extent=[0,1,0,1])
+
+    # show the 3D rotated projection
+    ax2.plot_surface(x, y, abs(matrix), rstride=1, cstride=1, cmap=plt.cm.jet,
+                                        linewidth=0, antialiased=False, shade=False)
+
+    plt.show()
+
+Espejo1 = EspejoResonador( curvatura=0.01)
+Espejo2 = EspejoResonador(curvatura=0.012)
 
 M1 = Espejo1.lente
 for i in range(0, 5):
@@ -20,23 +37,10 @@ x = np.arange(0, len(M1))
 y = np.arange(0, len(M1))
 x, y = np.meshgrid(x, y)
 
-# create the figure
-fig = plt.figure(figsize=plt.figaspect(0.5))
-ax1 = fig.add_subplot(1, 2, 1, projection='3d')
-ax2 = fig.add_subplot(1, 2, 2, projection='3d')
-
-# Mostrar la imagen rotandola
-#ax1.imshow(abs(rebote1), cmap=plt.cm.jet, interpolation='nearest',
-#               origin='lower', extent=[0,1,0,1])
-
-# show the 3D rotated projection
-ax1.plot_surface(x, y, abs(rebote1), rstride=1, cstride=1, cmap=plt.cm.jet,
-                                       linewidth=0, antialiased=False, shade=False)
-# Mostrar la imagen rotandola
-#ax3.imshow(abs(M1), cmap=plt.cm.jet, interpolation='nearest',
-#               origin='lower', extent=[0,1,0,1])
-
-# show the 3D rotated projection
-ax2.plot_surface(x, y, abs(M1), rstride=1, cstride=1, cmap=plt.cm.jet,
-                 linewidth=0, antialiased=False, shade=False)
-plt.show()
+threads = list()
+t = threading.Thread(target=mostrarEspejo, args=(x, y, M1))
+threads.append(t)
+t.start()
+w = threading.Thread(target=mostrarEspejo, args=(x, y, rebote1))
+threads.append(w)
+w.start()
