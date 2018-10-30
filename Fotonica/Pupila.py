5d931531a6052cf05c2b0eccd353e41158ef41bd
Jaimedgp <jaime.diez.gp@gmail.com>
2018-10-30 13:01:07 +0100

    Personalizar mas la pupila

--- Fotonica/Pupila.py
+++ Fotonica/Pupila.py
@@ -16,7 +16,7 @@ class EspejoResonador():
 
         self.lente = np.zeros((self.sizeX, self.sizeY), dtype=np.complex_)
 
-
+    def lenteCircular(self):
         for x in range(0, self.sizeX):
             rX = x-(self.sizeX/2)
             for y in range(0,self.sizeY):
@@ -26,6 +26,21 @@ class EspejoResonador():
                     self.lente[x][y] = cmath.exp(1j*self.crv*r2)
                 if r2 == (self.diamtr/2)**2:
                     self.lente[x][y] = 0.5*cmath.exp(1j*self.crv*r2)
+
+    def lenteCuadrada(self):
+        for x in range(0, self.sizeX):
+            rX = x-(self.sizeX/2)
+            for y in range(0,self.sizeY):
+                rY = (self.sizeY/2)-y
+                r2 = rX**2+rY**2
+
+                if abs(rX) < self.diamtr and abs(rY) < (self.diamtr:
+                    self.lente[x][y] = cmath.exp(1j*self.crv*r2)
+                elif ((abs(rX) < self.diamtr or abs(rY) < self.diamtr)
+                        and (abs(rX) == self.diamtr or abs(rY) == self.diamtr)):
+
+                        self.lente[x][y] = 0.5*cmath.exp(1j*self.crv*r2)
+
 
     def rebotes(self, imagen):
         M2 = imagen*self.lente
--- Fotonica/ResonadorTrue.py
+++ Fotonica/ResonadorTrue.py
@@ -4,7 +4,7 @@ import threading
 
 from Pupila import EspejoResonador
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.misc import imread, imresize, imrotate
+#from scipy.misc import imread, imresize, imrotate
 
 def mostrarEspejo(x, y, matrix):
     # create the figure
@@ -13,17 +13,24 @@ def mostrarEspejo(x, y, matrix):
     ax2 = fig.add_subplot(1, 2, 2, projection='3d')
 
     # Mostrar la imagen rotandola
-    ax1.imshow(abs(matrix), cmap=plt.cm.jet, interpolation='nearest',
-                origin='lower', extent=[0,1,0,1])
+    ax1.imshow(abs(matrix), cmap=plt.cm.jet)#, interpolation='nearest',
+                #origin='lower', extent=[0,1,0,1])
 
     # show the 3D rotated projection
     ax2.plot_surface(x, y, abs(matrix), rstride=1, cstride=1, cmap=plt.cm.jet,
                                         linewidth=0, antialiased=False, shade=False)
 
-    plt.show()
 
-Espejo1 = EspejoResonador( curvatura=0.01)
+Espejo1 = EspejoResonador(curvatura=0.01)
+Espejo1.lenteCircular()
 Espejo2 = EspejoResonador(curvatura=0.012)
+Espejo2.lenteCuadrada()
+
+fig = plt.figure()
+a = fig.add_subplot(121)
+b = fig.add_subplot(122)
+a.imshow(abs(Espejo1.lente))
+b.imshow(abs(Espejo2.lente))
 
 M1 = Espejo1.lente
 for i in range(0, 5):
@@ -37,10 +44,6 @@ x = np.arange(0, len(M1))
 y = np.arange(0, len(M1))
 x, y = np.meshgrid(x, y)
 
-threads = list()
-t = threading.Thread(target=mostrarEspejo, args=(x, y, M1))
-threads.append(t)
-t.start()
-w = threading.Thread(target=mostrarEspejo, args=(x, y, rebote1))
-threads.append(w)
-w.start()
+c = mostrarEspejo(x, y, rebote1)
+d = mostrarEspejo(x, y, M1)
+plt.show()
