55e7ec176d09bb4b68f3f306a298448ebfdf3d7d
Jaimedgp <jaime.diez.gp@gmail.com>
2018-10-31 11:16:40 +0100

    movimiento aleatorio de los espejos

--- Fotonica/Pupila.py
+++ Fotonica/Pupila.py
@@ -1,11 +1,11 @@
-import math
+import random
 import cmath
 import numpy as np
 
 class EspejoResonador():
 
     def __init__(self, tamanhoX=600, tamanhoY=600, diametro=120.0, curvatura=0,
-                 tilt=0, tip=0):
+                 tilt=0.0, tip=0.0):
 
         self.diamtr = diametro
         self.crv = curvatura
@@ -23,9 +23,12 @@ class EspejoResonador():
                 rY = (self.sizeY/2)-y
                 r2 = rX**2+rY**2
                 if r2 < (self.diamtr/2)**2:
-                    self.lente[x][y] = cmath.exp(1j*self.crv*r2)
+                    self.lente[x][y] = (cmath.exp(1j*self.crv*r2)*cmath.exp(1j*
+                                                (self.tip*rX + self.tilt*rY)))
+
                 if r2 == (self.diamtr/2)**2:
-                    self.lente[x][y] = 0.5*cmath.exp(1j*self.crv*r2)
+                    self.lente[x][y] = (0.5*cmath.exp(1j*self.crv*r2)*
+                                    cmath.exp(1j*(self.tip*rX + self.tilt*rY)))
 
     def lenteCuadrada(self):
         for x in range(0, self.sizeX):
@@ -48,4 +51,16 @@ class EspejoResonador():
 
         return reflec
 
+    def moverEspejoAleatorio(self):
+        self.tilt += np.random.randint(-10,10)/100.0
+        while self.tilt > 1:
+            self.tilt = self.tilt-2
+        while self.tilt < -1:
+            self.tilt = 2 - self.tilt
+        self.tip += np.random.randint(-10,10)/100.0
+        while self.tip > 1:
+            self.tip = self.tip-2
+        while self.tip < -1:
+            self.tip = 2 - self.tip
 
+        self.lenteCircular()
--- Fotonica/ResonadorTrue.py
+++ Fotonica/ResonadorTrue.py
@@ -4,13 +4,11 @@ import threading
 
 from Pupila import EspejoResonador
 from mpl_toolkits.mplot3d import Axes3D
-#from scipy.misc import imread, imresize, imrotate
 
-def mostrarEspejo(x, y, matrix):
+def mostrarEspejo(x, y, matrix, fig, i):
     # create the figure
-    fig = plt.figure(figsize=plt.figaspect(0.5))
-    ax1 = fig.add_subplot(1, 2, 1)
-    ax2 = fig.add_subplot(1, 2, 2, projection='3d')
+    ax1 = fig.add_subplot(3, 2, i)
+    ax2 = fig.add_subplot(3, 2, i+1, projection='3d')
 
     # Mostrar la imagen rotandola
     ax1.imshow(abs(matrix), cmap=plt.cm.jet)#, interpolation='nearest',
@@ -21,29 +19,42 @@ def mostrarEspejo(x, y, matrix):
                                         linewidth=0, antialiased=False, shade=False)
 
 
-Espejo1 = EspejoResonador(curvatura=0.01)
-Espejo1.lenteCuadrada()
-Espejo2 = EspejoResonador(curvatura=-0.012)
-Espejo2.lenteCuadrada()
+Espejo1 = EspejoResonador(curvatura=-0.01)
+Espejo1.lenteCircular()
+Espejo2 = EspejoResonador(curvatura=-0.01)
+Espejo2.lenteCircular()
 
 fig = plt.figure()
-a = fig.add_subplot(121)
-b = fig.add_subplot(122)
+a = fig.add_subplot(321)
+b = fig.add_subplot(322)
 a.imshow(abs(Espejo1.lente))
 b.imshow(abs(Espejo2.lente))
 
 M1 = Espejo1.lente
-for i in range(0, 50):
+for i in range(0, 10):
     rebote1 = Espejo1.rebotes(M1)
     if i == 0:
         rebote1 = np.fft.fftshift(rebote1)
+    Espejo1.moverEspejoAleatorio()
 
     M1 = Espejo2.rebotes(rebote1)
+    Espejo2.moverEspejoAleatorio()
+
+    print np.amax(abs(M1))
+
+
+print 'Curvatura final espejo 1: '
+print '\t'+'tilt = %.2f' %(Espejo1.tilt)
+print '\t'+'tip = %.2f' %(Espejo1.tip)
+
+print 'Curvatura final espejo 2: '
+print '\t'+'tilt = %.2f' %(Espejo2.tilt)
+print '\t'+'tip = %.2f' %(Espejo2.tip)
 
 x = np.arange(0, len(M1))
 y = np.arange(0, len(M1))
 x, y = np.meshgrid(x, y)
 
-c = mostrarEspejo(x, y, rebote1)
-d = mostrarEspejo(x, y, M1)
+c = mostrarEspejo(x, y, rebote1, fig, 3)
+d = mostrarEspejo(x, y, M1, fig, 5)
 plt.show()
