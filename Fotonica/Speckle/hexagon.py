5ea6d195184d59848d20ea66e09680b12858b206
Jaime <jaime.diez.gp@gmail.com>
2018-12-09 20:29:21 +0100

    funcion para definir el hexagono

--- Fotonica/Speckle/hexagon.py
+++ Fotonica/Speckle/hexagon.py
@@ -1,33 +1,34 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-ld = 50
-raiz3 = np.sqrt(3)
-altura = int(raiz3*ld/2)
-xSz = 2*altura
-ySz = 2*ld
+def hexagono(ld):
+    raiz3 = np.sqrt(3)
+    altura = int(raiz3*ld/2)
+    xSz = 2*altura
+    ySz = 2*ld
 
-lente = np.zeros((xSz, ySz))
+    lente = np.zeros((xSz, ySz))
 
-lente[:, ld/2:3*ld/2] = 1
+    lente[:, ld/2:3*ld/2] = 1
 
-for x in range(0, ld/2):
-    rX = x
-    yTeo1 = (-raiz3) * rX + raiz3*25
-    yTeo2 = (raiz3) * rX
+    for x in range(0, ld/2):
+        rX = x
+        yTeo1 = (-raiz3) * rX + raiz3*25
+        yTeo2 = (raiz3) * rX
 
-    for y in range(0, altura):
-        rY = altura - y
+        for y in range(0, altura):
+            rY = altura - y
 
-        # Arriba a la derecha
-        if rY < yTeo1:
-            lente[y][x+(3*ld/2)] = 1
-        # Abajo a la derecha
-        if rY > yTeo2:
-            lente[altura+y][x+(3*ld/2)] = 1
-        # Arriba a la Izquierda
-        if rY < yTeo2:
-            lente[y][x] = 1
-        # Abajo a la izquierda
-        if rY > yTeo1:
-            lente[altura+y][x] = 1
+            # Arriba a la derecha
+            if rY < yTeo1:
+                lente[y][x+(3*ld/2)] = 1
+            # Abajo a la derecha
+            if rY > yTeo2:
+                lente[altura+y][x+(3*ld/2)] = 1
+            # Arriba a la Izquierda
+            if rY < yTeo2:
+                lente[y][x] = 1
+            # Abajo a la izquierda
+            if rY > yTeo1:
+                lente[altura+y][x] = 1
+
+    return lente
