8d17ab562382279b21294dc2d7eed8687e0bb507
Jaime <jaime.diez.gp@gmail.com>
2019-02-20 17:19:17 +0100

    Programa madelung en 2-D para varias zonas

--- CuanticaIII/Madelung/EvjenNaCl.py
+++ CuanticaIII/Madelung/EvjenNaCl.py
@@ -2,18 +2,20 @@
     Programa para la obtencion de la constante de Madelung para una esctructura
     2-D de NaCl mediante el procedimiento de Evjen. se realiza el calculo
     iterativo hasta que la contribucion de la zona sea menor que <0.001
+
+    (x, y, z) => (k, j, i) siendo z la profundidad, y la altura y x la anchura
 """
 
 import numpy as np
 
 def crearRed(zona):
     length = zona+1
-    red = np.zeros((length, length, 1))
+    red = np.zeros((1, length, length))
 
-    for i in range(length):
+    for i in range(1):
         for j in range(length):
-            for k in range(1):
-                xyz = i+j+k
+            for k in range(length):
+                xyz = (i+j+k)/zona
 
                 if xyz != 0:
                     red[i][j][k] += 1/(2.0**xyz)
@@ -22,24 +24,35 @@ def crearRed(zona):
 def calcMadelung(red):
     madelung = 0
 
-    for i in range(len(red)):
-        for j in range(len(red)):
-            k = 0
+    for j in range(len(red[0])):
+        for k in range(1, len(red[0][0])):
+            i = 0
             if i == len(red):
                 pij2 = i*i+j*j+k*k
+                xyz = i+j+k
 
-                if pij2 % 2 == 0:
+                if xyz % 2 == 0:
                     madelung += red[i][j][k]/np.sqrt(pij2)
                 else:
                     madelung -= red[i][j][k]/np.sqrt(pij2)
-            elif (j == len(red)-1 or k == len(red)-1):
+            elif (j == len(red[0])-1 or k == len(red[0][0])-1):
                 pij2 = i*i+j*j+k*k
+                xyz = i+j+k
 
-                if pij2 % 2 == 0:
+                if xyz % 2 == 0:
                     madelung -= red[i][j][k]/np.sqrt(pij2)
                 else:
                     madelung += red[i][j][k]/np.sqrt(pij2)
     return 4*madelung
 
-red = crearRed(1)
-print calcMadelung(red)
+madelung = 0
+zonas = [1,2,3]
+
+for zona in zonas:
+    if zona > 1:
+        red = 1- red
+        madelung += calcMadelung(red)
+
+    red = crearRed(zona)
+    madelung += calcMadelung(red)
+    print madelung
